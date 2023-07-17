798974d0761ea98e8ae7005214bb2d0572138d5c
moreaudamien <128502618+moreaudamien@users.noreply.github.com>
2023-07-17 22:29:09 +0200

    Update DoctorController.java

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -3,14 +3,21 @@ package com.example.thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class DoctorController {
 
     @GetMapping("/doctor/")
-    public String doctor(@RequestParam(required=false, defaultValue="0") int number,
-                         @RequestParam(required=false, defaultValue="John Smith") String name) {
+    public String doctor(@RequestParam(required = false, defaultValue = "0") int number,
+            @RequestParam(required = false, defaultValue = "John Smith") String name, Model model) {
+        model.addAttribute("number", number);
+        model.addAttribute("name", name);
 
         return "doctor";
+    }
+
+    private int extracted(int number) {
+        return number;
     }
 }
